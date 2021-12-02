f2a698ab65718429c5fe6784cc38f3300214a6b2
Romain BRIARD <robriard@e2r2p11.clusters.42paris.fr>
2021-12-02 17:31:19 +0100

    CPP07

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: unknow <unknow@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 15:03:43 by unknow            #+#    #+#             */
-/*   Updated: 2021/11/25 16:19:35 by unknow           ###   ########.fr       */
+/*   Updated: 2021/12/02 17:27:08 by robriard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,6 @@ int main(void)
 	std::cout << std::endl;
 	::iter(test, strlen(test), add);
 	::iter(test, strlen(test), print);
+	std::cout << std::endl;
 	return 0;
 }
